0e26af8cab90a48f1f40544b1abaa7cd6e361e19
fangzh <fangzh@otitan.com>
2023-11-10 11:05:59 +0800

    1、扫码ui优化

---
+++ common/src/main/java/pers/fz/mvvm/wight/customlayout/CirclePaddingImageView.java
@@ -0,0 +1,114 @@
+package pers.fz.mvvm.wight.customlayout;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.StateListDrawable;
+import android.util.AttributeSet;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageView;
+
+import pers.fz.mvvm.R;
+
+/**
+ * Created by fz on 2023/11/10 8:57
+ * describe :
+ */
+public class CirclePaddingImageView extends AppCompatImageView {
+
+    private int borderColor;
+    private int borderFocusColor;
+    private boolean enableSelected;
+    private boolean enablePressed;
+    private int borderWidth;
+    private GradientDrawable bgFocusedDrawable;
+    private GradientDrawable bgDefaultDrawable;
+
+    public CirclePaddingImageView(Context context) {
+        super(context);
+        init(null);
+    }
+
+    public CirclePaddingImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public CirclePaddingImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    private void init(@Nullable AttributeSet attrs) {
+        if (attrs != null) {
+            // 从XML中获取自定义属性
+            TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.CircleImageView);
+            borderColor = a.getColor(R.styleable.CircleImageView_border_color, 0xFFB4B4B4);
+            borderFocusColor = a.getColor(R.styleable.CircleImageView_border_focus_color, 0xFFFFFFFF);
+            borderWidth = a.getDimensionPixelSize(R.styleable.CircleImageView_border_width, 0);
+            enableSelected = a.getBoolean(R.styleable.CircleImageView_enable_selected, false);
+            enablePressed = a.getBoolean(R.styleable.CircleImageView_enable_pressed, false);
+            a.recycle();
+        }
+
+        bgDefaultDrawable = new GradientDrawable();
+        // 设置背景为圆形
+        bgDefaultDrawable.setShape(GradientDrawable.OVAL);
+        bgDefaultDrawable.setStroke(borderWidth, borderColor);
+
+        bgFocusedDrawable = new GradientDrawable();
+        bgFocusedDrawable.setShape(GradientDrawable.OVAL);
+        bgFocusedDrawable.setStroke(borderWidth, borderFocusColor);
+        setBackground(bgDefaultDrawable);
+    }
+
+    @Override
+    public void setSelected(boolean selected) {
+        super.setSelected(selected);
+        if (!enableSelected) {
+            return;
+        }
+        if (selected) {
+            setBackground(bgFocusedDrawable);
+        } else {
+            setBackground(bgDefaultDrawable);
+        }
+    }
+
+    @Override
+    public void setPressed(boolean pressed) {
+        super.setPressed(pressed);
+        if (!enableSelected && !enablePressed) {
+            return;
+        }
+        if (enablePressed) {
+            if (pressed) {
+                setBackground(bgFocusedDrawable);
+            } else {
+                setBackground(bgDefaultDrawable);
+            }
+            return;
+        }
+        if (pressed) {
+            setBackground(bgFocusedDrawable);
+        }
+    }
+
+    public void setBorderColor(int color) {
+        borderColor = color;
+        invalidate();
+    }
+
+    public void setBorderWidth(int width) {
+        borderWidth = width;
+        invalidate();
+    }
+
+}
