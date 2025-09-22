c07a95effa14582a25fa14379cde562d192174dd
fangzh <fangzh@otitan.com>
2025-09-22 14:55:12 +0800

    1、全新库发布 2、统一各个库的包名

--- commonui/src/test/java/com/casic/otitan/commonui/ExampleUnitTest.java
+++ commonui/src/test/java/com/casic/otitan/commonui/ExampleUnitTest.java
@@ -1,8 +1,5 @@
 package com.casic.otitan.commonui;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +7,4 @@ import org.junit.Test;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
 }
