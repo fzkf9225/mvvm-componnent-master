9c694a93c1fdbc387d7ff109439b8713f8203621
fangzh <fangzh@otitan.com>
2026-03-31 14:40:15 +0800

    优化自定义View

--- common/src/main/java/com/casic/otitan/common/widget/customview/CornerButton.java
+++ common/src/main/java/com/casic/otitan/common/widget/customview/CornerButton.java
@@ -15,7 +15,7 @@ import com.casic.otitan.common.R;
 
 /**
  * Created by fz on 2019/5/31.
- * describe：自定义圆角矩形
+ * describe：自定义圆角矩形（支持分别设置四个圆角）
  */
 public class CornerButton extends AppCompatButton {
     /**
@@ -31,9 +31,33 @@ public class CornerButton extends AppCompatButton {
      */
     protected GradientDrawable gradientDrawable;
     /**
-     * 圆角半径和边框宽度
+     * 圆角半径（四个角统一使用）
      */
-    protected float radius, strokeWidth;
+    protected float radius;
+    /**
+     * 边框宽度
+     */
+    protected float strokeWidth;
+    /**
+     * 左上角圆角半径
+     */
+    protected float leftTopRadius;
+    /**
+     * 右上角圆角半径
+     */
+    protected float rightTopRadius;
+    /**
+     * 右下角圆角半径
+     */
+    protected float rightBottomRadius;
+    /**
+     * 左下角圆角半径
+     */
+    protected float leftBottomRadius;
+    /**
+     * 是否设置了描边
+     */
+    protected boolean hasStroke = false;
 
     public CornerButton(Context context) {
         this(context, null);
@@ -41,82 +65,184 @@ public class CornerButton extends AppCompatButton {
 
     public CornerButton(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
         if (attrs != null) {
             TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CornerTextView);
-            strokeColor = typedArray.getColor(R.styleable.CornerTextView_strokeColor, ContextCompat.getColor(context, R.color.white));
-            circleBackColor = typedArray.getColor(R.styleable.CornerTextView_bgColor, ContextCompat.getColor(context, R.color.white));
-            strokeWidth = typedArray.getDimension(R.styleable.CornerTextView_strokeWidth, strokeWidth);
+
+            strokeColor = typedArray.getColor(R.styleable.CornerTextView_strokeColor,
+                    ContextCompat.getColor(context, R.color.white));
+            circleBackColor = typedArray.getColor(R.styleable.CornerTextView_bgColor,
+                    ContextCompat.getColor(context, R.color.white));
+            strokeWidth = typedArray.getDimension(R.styleable.CornerTextView_strokeWidth, 0);
+
+            // 读取统一的圆角半径
             radius = typedArray.getDimension(R.styleable.CornerTextView_radius, 0);
+
+            // 读取四个角的单独设置
+            leftTopRadius = typedArray.getDimension(R.styleable.CornerTextView_leftTopRadius, radius);
+            rightTopRadius = typedArray.getDimension(R.styleable.CornerTextView_rightTopRadius, radius);
+            rightBottomRadius = typedArray.getDimension(R.styleable.CornerTextView_rightBottomRadius, radius);
+            leftBottomRadius = typedArray.getDimension(R.styleable.CornerTextView_leftBottomRadius, radius);
+
             typedArray.recycle();
         } else {
             strokeColor = ContextCompat.getColor(context, R.color.white);
             circleBackColor = ContextCompat.getColor(context, R.color.white);
+            radius = 0;
+            leftTopRadius = radius;
+            rightTopRadius = radius;
+            rightBottomRadius = radius;
+            leftBottomRadius = radius;
         }
+
+        hasStroke = strokeWidth > 0;
+        applyBackground();
+    }
+
+    /**
+     * 应用背景（圆角+描边）
+     */
+    private void applyBackground() {
         gradientDrawable = new GradientDrawable();
         gradientDrawable.setColor(circleBackColor);
-        gradientDrawable.setCornerRadius(radius);
-        if (strokeWidth > 0) {
+
+        // 设置圆角
+        if (leftTopRadius == radius && rightTopRadius == radius &&
+                rightBottomRadius == radius && leftBottomRadius == radius) {
+            gradientDrawable.setCornerRadius(radius);
+        } else {
+            float[] radii = new float[]{
+                    leftTopRadius, leftTopRadius,
+                    rightTopRadius, rightTopRadius,
+                    rightBottomRadius, rightBottomRadius,
+                    leftBottomRadius, leftBottomRadius
+            };
+            gradientDrawable.setCornerRadii(radii);
+        }
+
+        // 设置描边
+        if (hasStroke) {
             gradientDrawable.setStroke((int) strokeWidth, strokeColor);
         }
+
         this.setBackground(gradientDrawable);
     }
 
     public void setStrokeColor(@ColorInt int strokeColor) {
         this.strokeColor = strokeColor;
-        gradientDrawable.setStroke((int) strokeWidth, this.strokeColor);
-        this.setBackground(gradientDrawable);
+        this.hasStroke = strokeWidth > 0;
+        applyBackground();
     }
 
-    public void setStrokeWidth(int strokeWidth) {
+    public void setStrokeWidth(float strokeWidth) {
         this.strokeWidth = strokeWidth;
-        gradientDrawable.setStroke(this.strokeColor, strokeColor);
-        this.setBackground(gradientDrawable);
+        this.hasStroke = strokeWidth > 0;
+        applyBackground();
     }
 
-    public void setStroke(@ColorInt int strokeColor, int strokeWidth) {
+    public void setStroke(@ColorInt int strokeColor, float strokeWidth) {
         this.strokeColor = strokeColor;
         this.strokeWidth = strokeWidth;
-        gradientDrawable.setStroke(this.strokeColor, this.strokeColor);
-        this.setBackground(gradientDrawable);
+        this.hasStroke = strokeWidth > 0;
+        applyBackground();
     }
 
     public void setBackColor(@ColorInt int color) {
         this.circleBackColor = color;
-        gradientDrawable.setColor(circleBackColor);
-        this.setBackground(gradientDrawable);
+        applyBackground();
     }
 
     public void setBackColor(ColorStateList bgColor) {
-        gradientDrawable.setColor(bgColor);
+        // GradientDrawable 不支持 ColorStateList，这里做转换处理
+        if (bgColor != null && !bgColor.isStateful()) {
+            gradientDrawable.setColor(bgColor.getDefaultColor());
+        } else if (bgColor != null) {
+            gradientDrawable.setColor(bgColor.getDefaultColor());
+        }
         this.setBackground(gradientDrawable);
     }
 
+    /**
+     * 统一设置圆角半径
+     */
     public void setRadius(float radius) {
         this.radius = radius;
-        gradientDrawable.setCornerRadius(this.radius);
-        this.setBackground(gradientDrawable);
+        this.leftTopRadius = radius;
+        this.rightTopRadius = radius;
+        this.rightBottomRadius = radius;
+        this.leftBottomRadius = radius;
+        applyBackground();
+    }
+
+    /**
+     * 分别设置四个角的圆角半径
+     */
+    public void setCornerRadii(float leftTop, float rightTop, float rightBottom, float leftBottom) {
+        this.leftTopRadius = leftTop;
+        this.rightTopRadius = rightTop;
+        this.rightBottomRadius = rightBottom;
+        this.leftBottomRadius = leftBottom;
+        applyBackground();
     }
 
     public void setBgColorAndRadius(@ColorInt int bgColor, float radius) {
         this.radius = radius;
+        this.leftTopRadius = radius;
+        this.rightTopRadius = radius;
+        this.rightBottomRadius = radius;
+        this.leftBottomRadius = radius;
         this.circleBackColor = bgColor;
-        gradientDrawable.setColor(this.circleBackColor);
-        gradientDrawable.setCornerRadius(this.radius);
-        this.setBackground(gradientDrawable);
+        applyBackground();
     }
 
-    public void setButtonStyle(@ColorInt int strokeColor, int strokeWidth, @ColorInt int bgColor, float radius) {
+    /**
+     * 设置背景颜色和四个角的圆角半径
+     */
+    public void setBgColorAndCornerRadii(@ColorInt int bgColor, float leftTop, float rightTop,
+                                         float rightBottom, float leftBottom) {
+        this.circleBackColor = bgColor;
+        this.leftTopRadius = leftTop;
+        this.rightTopRadius = rightTop;
+        this.rightBottomRadius = rightBottom;
+        this.leftBottomRadius = leftBottom;
+        applyBackground();
+    }
+
+    public void setButtonStyle(@ColorInt int strokeColor, float strokeWidth,
+                               @ColorInt int bgColor, float radius) {
         this.strokeColor = strokeColor;
         this.strokeWidth = strokeWidth;
         this.radius = radius;
+        this.leftTopRadius = radius;
+        this.rightTopRadius = radius;
+        this.rightBottomRadius = radius;
+        this.leftBottomRadius = radius;
         this.circleBackColor = bgColor;
-        gradientDrawable.setColor(this.circleBackColor);
-        gradientDrawable.setCornerRadius(this.radius);
-        this.setBackground(gradientDrawable);
+        this.hasStroke = strokeWidth > 0;
+        applyBackground();
     }
 
     public void setGradientDrawable(GradientDrawable gradientDrawable) {
         this.gradientDrawable = gradientDrawable;
         this.setBackground(gradientDrawable);
+    }
+
+    public float getLeftTopRadius() {
+        return leftTopRadius;
+    }
+
+    public float getRightTopRadius() {
+        return rightTopRadius;
+    }
+
+    public float getRightBottomRadius() {
+        return rightBottomRadius;
+    }
+
+    public float getLeftBottomRadius() {
+        return leftBottomRadius;
     }
 }
--- common/src/main/java/com/casic/otitan/common/widget/customview/CornerTextView.java
+++ common/src/main/java/com/casic/otitan/common/widget/customview/CornerTextView.java
@@ -14,7 +14,7 @@ import com.casic.otitan.common.R;
 
 /**
  * Created by fz on 2019/5/31.
- * describe：自定义圆角矩形
+ * describe：自定义圆角矩形（支持分别设置四个圆角）
  */
 public class CornerTextView extends AppCompatTextView {
     /**
@@ -30,9 +30,29 @@ public class CornerTextView extends AppCompatTextView {
      */
     protected float strokeWidth;
     /**
-     * 圆角半径
+     * 圆角半径（四个角统一使用）
      */
     protected float radius;
+    /**
+     * 左上角圆角半径
+     */
+    protected float leftTopRadius;
+    /**
+     * 右上角圆角半径
+     */
+    protected float rightTopRadius;
+    /**
+     * 右下角圆角半径
+     */
+    protected float rightBottomRadius;
+    /**
+     * 左下角圆角半径
+     */
+    protected float leftBottomRadius;
+    /**
+     * 是否设置了描边
+     */
+    protected boolean hasStroke = false;
     /**
      * 圆角矩形
      */
@@ -44,49 +64,89 @@ public class CornerTextView extends AppCompatTextView {
 
     public CornerTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
         if (attrs != null) {
             TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CornerTextView);
-            strokeColor = typedArray.getColor(R.styleable.CornerTextView_strokeColor, ContextCompat.getColor(context, R.color.white));
-            circleBackColor = typedArray.getColor(R.styleable.CornerTextView_bgColor, ContextCompat.getColor(context, R.color.white));
+
+            strokeColor = typedArray.getColor(R.styleable.CornerTextView_strokeColor,
+                    ContextCompat.getColor(context, R.color.white));
+            circleBackColor = typedArray.getColor(R.styleable.CornerTextView_bgColor,
+                    ContextCompat.getColor(context, R.color.white));
             strokeWidth = typedArray.getDimension(R.styleable.CornerTextView_strokeWidth, 0);
+
+            // 读取统一的圆角半径
             radius = typedArray.getDimension(R.styleable.CornerTextView_radius, 0);
+
+            // 读取四个角的单独设置
+            leftTopRadius = typedArray.getDimension(R.styleable.CornerTextView_leftTopRadius, radius);
+            rightTopRadius = typedArray.getDimension(R.styleable.CornerTextView_rightTopRadius, radius);
+            rightBottomRadius = typedArray.getDimension(R.styleable.CornerTextView_rightBottomRadius, radius);
+            leftBottomRadius = typedArray.getDimension(R.styleable.CornerTextView_leftBottomRadius, radius);
+
             typedArray.recycle();
         } else {
             strokeColor = ContextCompat.getColor(context, R.color.white);
             circleBackColor = ContextCompat.getColor(context, R.color.white);
+            radius = 0;
+            leftTopRadius = radius;
+            rightTopRadius = radius;
+            rightBottomRadius = radius;
+            leftBottomRadius = radius;
         }
+
+        hasStroke = strokeWidth > 0;
+        applyBackground();
+    }
+
+    /**
+     * 应用背景（圆角+描边）
+     */
+    private void applyBackground() {
+        gradientDrawable = new GradientDrawable();
         gradientDrawable.setColor(circleBackColor);
-        gradientDrawable.setCornerRadius(radius);
-        if (strokeWidth > 0) {
+
+        // 设置圆角
+        if (leftTopRadius == radius && rightTopRadius == radius &&
+                rightBottomRadius == radius && leftBottomRadius == radius) {
+            // 所有圆角相同，使用统一的圆角半径
+            gradientDrawable.setCornerRadius(radius);
+        } else {
+            // 分别设置四个角的圆角半径
+            float[] radii = new float[]{
+                    leftTopRadius, leftTopRadius,      // 左上角 x, y
+                    rightTopRadius, rightTopRadius,    // 右上角 x, y
+                    rightBottomRadius, rightBottomRadius, // 右下角 x, y
+                    leftBottomRadius, leftBottomRadius    // 左下角 x, y
+            };
+            gradientDrawable.setCornerRadii(radii);
+        }
+
+        // 设置描边
+        if (hasStroke) {
             gradientDrawable.setStroke((int) strokeWidth, strokeColor);
         }
+
         this.setBackground(gradientDrawable);
     }
 
     public void setBackColor(@ColorInt int color) {
         this.circleBackColor = color;
-        gradientDrawable.setColor(circleBackColor);
-        gradientDrawable.setCornerRadius(radius);
-        this.setBackground(gradientDrawable);
+        applyBackground();
     }
 
     public void setStroke(int strokeWidth, int color) {
         this.strokeColor = color;
         this.strokeWidth = strokeWidth;
-        gradientDrawable.setColor(circleBackColor);
-        gradientDrawable.setCornerRadius(radius);
-        if (strokeWidth > 0) {
-            gradientDrawable.setStroke(strokeWidth, strokeColor);
-        }
-        this.setBackground(gradientDrawable);
+        this.hasStroke = strokeWidth > 0;
+        applyBackground();
     }
-
 
     public void setBgColor(int color) {
         this.circleBackColor = color;
-        gradientDrawable.setColor(this.circleBackColor);
-        gradientDrawable.setCornerRadius(this.radius);
-        this.setBackground(gradientDrawable);
+        applyBackground();
     }
 
     public void setGradientDrawable(GradientDrawable gradientDrawable) {
@@ -94,18 +154,50 @@ public class CornerTextView extends AppCompatTextView {
         this.setBackground(this.gradientDrawable);
     }
 
+    /**
+     * 统一设置圆角半径
+     */
     public void setRadius(float radius) {
         this.radius = radius;
-        gradientDrawable.setCornerRadius(this.radius);
-        this.setBackground(gradientDrawable);
+        this.leftTopRadius = radius;
+        this.rightTopRadius = radius;
+        this.rightBottomRadius = radius;
+        this.leftBottomRadius = radius;
+        applyBackground();
+    }
+
+    /**
+     * 分别设置四个角的圆角半径
+     */
+    public void setCornerRadii(float leftTop, float rightTop, float rightBottom, float leftBottom) {
+        this.leftTopRadius = leftTop;
+        this.rightTopRadius = rightTop;
+        this.rightBottomRadius = rightBottom;
+        this.leftBottomRadius = leftBottom;
+        applyBackground();
     }
 
     public void setBgColorAndRadius(int color, float radius) {
         this.radius = radius;
+        this.leftTopRadius = radius;
+        this.rightTopRadius = radius;
+        this.rightBottomRadius = radius;
+        this.leftBottomRadius = radius;
         this.circleBackColor = color;
-        gradientDrawable.setColor(this.circleBackColor);
-        gradientDrawable.setCornerRadius(this.radius);
-        this.setBackground(gradientDrawable);
+        applyBackground();
+    }
+
+    /**
+     * 设置背景颜色和四个角的圆角半径
+     */
+    public void setBgColorAndCornerRadii(int color, float leftTop, float rightTop,
+                                         float rightBottom, float leftBottom) {
+        this.circleBackColor = color;
+        this.leftTopRadius = leftTop;
+        this.rightTopRadius = rightTop;
+        this.rightBottomRadius = rightBottom;
+        this.leftBottomRadius = leftBottom;
+        applyBackground();
     }
 
     public float getRadius() {
@@ -122,5 +214,21 @@ public class CornerTextView extends AppCompatTextView {
 
     public int getCircleBackColor() {
         return circleBackColor;
+    }
+
+    public float getLeftTopRadius() {
+        return leftTopRadius;
+    }
+
+    public float getRightTopRadius() {
+        return rightTopRadius;
+    }
+
+    public float getRightBottomRadius() {
+        return rightBottomRadius;
+    }
+
+    public float getLeftBottomRadius() {
+        return leftBottomRadius;
     }
 }
