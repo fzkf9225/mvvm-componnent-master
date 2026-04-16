df2815e5853fbef2b400710d067fecbbf718997c
fangzh <fangzh@otitan.com>
2026-04-16 10:14:36 +0800

    包名重构全新的依赖库和包名 4.0版本

--- annotation/src/androidTest/java/com/casic/titan/annotation/ExampleInstrumentedTest.java
+++ annotation/src/androidTest/java/com/casic/titan/annotation/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.casic.otitan.annotation.test", appContext.getPackageName());
+        assertEquals("io.coderf.arklab.annotation.test", appContext.getPackageName());
     }
 }
