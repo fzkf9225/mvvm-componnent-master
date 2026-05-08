23a185756a4c0a8c5075c3a597cae49e7d8d6695
fangzh <fangzh@otitan.com>
2026-05-08 11:47:22 +0800

    1、以登录为示例新增domin+useCase模块示例代码 2、优化登录ViewModel中混杂这UI等等混乱代码的问题

---
+++ usercomponent/src/main/java/io/coderf/arklab/usercomponent/domain/repository/UserProfileRepository.kt
@@ -0,0 +1,14 @@
+package io.coderf.arklab.usercomponent.domain.repository
+
+import androidx.lifecycle.MutableLiveData
+import io.coderf.arklab.userapi.bean.UserInfo
+import io.reactivex.rxjava3.disposables.Disposable
+
+/**
+ * 用户资料相关数据端口；由 [io.coderf.arklab.usercomponent.repository.UserRepositoryImpl] 实现，
+ * [io.coderf.arklab.usercomponent.domain.usecase.RefreshUserProfileUseCase] 只依赖本接口以便替换与测试。
+ */
+interface UserProfileRepository {
+
+    fun refreshUserInfo(liveData: MutableLiveData<UserInfo>): Disposable
+}
